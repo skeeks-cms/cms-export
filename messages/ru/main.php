cbbf6831a9c8d93dc008dadc3814d13bee9cf14f
Semenov <semenov@skeeks.com>
2016-09-02 17:16:41 +0300

    dev

--- messages/ru/main.php
+++ messages/ru/main.php
@@ -4,7 +4,7 @@ return [
     'Export' => 'Экспорт',
     'Name' => 'Название',
     'Description' => 'Описание',
-    'Tasks on exports' => 'Задачи по импорту',
+    'Tasks on exports' => 'Задачи по экспорту',
     'Component' => 'Обработчик',
     'All kinds of exports' => 'Все виды экспорта',
 ];
